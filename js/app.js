b51d3ba2d678e2a0219e5fb5ee2273fb84680f18
Maria Fernanda <mf.dm1998@gmail.com>
2024-08-05 03:21:50 -0400

    Create funcion desencriptar y desencriptarTexto

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@ function validarTexto(texto) {
 }
 
 function encriptar(texto) {
-    const vocales = [["a", "ai"], ["e", "enter"], ["i", "imes"], ["o", "ober"], ["u", "ufat"]];
+    const vocales = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
     let textoEncriptado = texto.toLowerCase();
 
     for (let i = 0; i < vocales.length; i++) {
@@ -28,5 +28,28 @@ function encriptarTexto() {
         alert('Texto encriptado');
     } else {
         alert('El texto contiene caracteres inválidos. Use solo letras minúsculas sin acentos.');
+    }
+}
+
+
+function desencriptar(texto) {
+    const vocales = [["enter", "e"], ["imes", "i"], ["ai", "a"], ["ober", "o"], ["ufat", "u"]];
+    let textoDesencriptado = texto.toLowerCase();
+
+    for (let i = 0; i < vocales.length; i++) {
+        textoDesencriptado = textoDesencriptado.split(vocales[i][0]).join(vocales[i][1]);
+    }
+    return textoDesencriptado;
+}
+
+function desencriptarTexto() {
+    const texto = textoEntrada.value;
+    if (validarTexto(texto)) {
+        const textoDesencriptado = desencriptar(texto);
+        textoSalida.value = textoDesencriptado;
+        textoEntrada.value = "";
+        alert('Texto desencriptado');
+    } else {
+        alert("El texto contiene caracteres invalidos. Use solo letras minúsculas sin acento");
     }
 }
